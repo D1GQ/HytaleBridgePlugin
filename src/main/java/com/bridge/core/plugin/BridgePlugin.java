877da38098b7939ea4e1154e525fd76027b17d7f
D1GQ <yourboylax@gmail.com>
2026-01-18 20:15:24 -0600

    remove test command

--- src/main/java/com/bridge/core/plugin/BridgePlugin.java
+++ src/main/java/com/bridge/core/plugin/BridgePlugin.java
@@ -1,21 +1,14 @@
 package com.bridge.core.plugin;
 
 import com.bridge.api.utilities.BridgeUtil;
-import com.bridge.core.commands.TestCommand;
 import com.bridge.core.data.DataManager;
 import com.bridge.core.factory.BridgeWorldFactory;
 import com.bridge.core.systems.BridgeWorldManagerSystem;
 import com.bridge.core.systems.PlayerDeathSystem;
 import com.bridge.core.systems.PlayerJoinAndLeaveSystem;
 import com.hypixel.hytale.logger.HytaleLogger;
-import com.hypixel.hytale.server.core.HytaleServer;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
-import com.hypixel.hytale.server.core.universe.Universe;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Nonnull;
 
@@ -39,7 +32,6 @@ public final class BridgePlugin extends JavaPlugin {
         logger = getLogger();
         DataManager.init(getDataDirectory());
         registerSystems();
-        getCommandRegistry().registerCommand(new TestCommand("test", "An test command"));
 
         BridgeUtil.getDefaultWorldReady().thenRunAsync(() -> {
             BridgeWorldFactory.load();
